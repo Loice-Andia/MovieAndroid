cb1c823b468897f7278eef50b5ebaf1b3b845558
chikwesh <lolo@enuma.com>
2018-09-21 10:09:19 +0300

    [Module] Create view packages

---
+++ app/src/main/java/com/andia/loice/movies/view/activity/MovieListActivity.java
@@ -0,0 +1,15 @@
+package com.andia.loice.movies.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.andia.loice.movies.R;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+    }
+}
