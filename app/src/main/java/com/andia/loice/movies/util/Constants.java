d8eb4fc9a76e49659dc6c42d1f5956a89475186f
chikwesh <lolo@enuma.com>
2018-09-21 09:48:32 +0300

    [Module] Add network module which utilizes rxjava2 and retrofit

--- app/src/main/java/com/andia/loice/movies/util/Constants.java
+++ app/src/main/java/com/andia/loice/movies/util/Constants.java
@@ -2,4 +2,5 @@ package com.andia.loice.movies.util;
 
 public class Constants {
     public static final String DB_NAME = "db-fullstop";
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
 }
